630f677af87f41af8590d5606a7ce31747da0870
Hana T <hana@gplusmedia.com>
2022-10-02 23:11:12 +0900

    update class

--- app/Views/final-slide.php
+++ app/Views/final-slide.php
@@ -2,6 +2,16 @@
 
 <?= $this->section('content') ?>
 
+    <section>
+        <p>Drive API Quickstart</p>
+
+        <!--Add buttons to initiate auth sequence and sign out-->
+        <button id="authorize_button" onclick="handleAuthClick()">Authorize</button>
+        <button id="signout_button" onclick="handleSignoutClick()">Sign Out</button>
+
+        <pre id="content" style="white-space: pre-wrap;"></pre>
+    </section>
+
     <!-- CONTENT -->
     <section class="section">
 
@@ -21,7 +31,7 @@
 
         <div class="field">
             <?= form_label('Slides', 'slides', ['class' => 'label']) ?>
-            <div class="control">
+            <div id="gdrive-list" class="box">
             </div>
         </div>
 
@@ -45,6 +55,8 @@
 
     <script src="https://cdn.jsdelivr.net/gh/gitbrent/pptxgenjs@3.4.0/dist/pptxgen.bundle.js"></script>
 <?php $assets = json_decode(file_get_contents(__DIR__ . '/../../public/manifest.json')) ?>
-    <script src="<?= base_url($assets->ppt->js) ?>"></script>
+    <script src="<?= base_url($assets->gdrive->js) ?>"></script>
+    <script async defer src="https://apis.google.com/js/api.js"></script>
+    <script async defer src="https://accounts.google.com/gsi/client"></script>
 
 <?= $this->endSection() ?>
